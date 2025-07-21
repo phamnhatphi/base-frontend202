a979a6a3d648abc3d3b93b1c4497c3988bd0b110
GLODEIT\Nhu.HTT <nhu.htt@globaldesignit.vn>
2025-07-21 10:34:33 +0700

    auto import

--- src/runtime/components/ButtonABC.vue
+++ src/runtime/components/ButtonABC.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    ABC
-  </div>
+  <v-btn
+    class="text-none mb-4"
+    color="indigo-darken-3"
+    size="x-large"
+    variant="flat"
+    block
+    @click="con"
+  >
+    Verify and continue
+  </v-btn>
 </template>
+
+<script setup lang="ts">
+const con = () => {
+  console.log('eeeeeeeeeeeeee')
+}
+</script>
+
+<style scoped>
+</style>
